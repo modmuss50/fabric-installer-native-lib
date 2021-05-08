62e598849444f80ac0402320ae4eca425be1b832
modmuss50 <modmuss50@gmail.com>
2021-05-08 18:37:12 +0100

    Windows and github actions

--- windows.cpp
+++ windows.cpp
@@ -1,6 +1,18 @@
 #include "MojangLauncherHelper.h"
 
+#include <windows.h>
+
+constexpr LPCSTR MinecraftLauncherMutexName = "MojangLauncher";
+
 jboolean Java_net_fabricmc_installer_launcher_MojangLauncherHelper_isMojangLauncherOpen(JNIEnv *, jclass) {
-    // TODO
+    HANDLE handle = CreateMutexA(nullptr, false, MinecraftLauncherMutexName);
+
+    if (GetLastError() == ERROR_ALREADY_EXISTS)
+    {
+        // Already exists so must be open.
+        return 1;
+    }
+
+    CloseHandle(handle);
     return 0;
 }
